574f1f08f1ec84fdbaf7dc3ba79d0aa3b698bc5d
Ayan <foxtrotsin9@gmail.com>
2023-04-20 14:26:43 -0400

    Feature 7 complete

--- expenses/urls.py
+++ expenses/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("receipts/", include("receipts.urls")),
     path("", redirect_to_receipt_list),
+    path("accounts/", include("accounts.urls")),
 ]
